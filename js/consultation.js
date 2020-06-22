ca16573fb7217cd8e2395ea2c471a91ec90b5e7b
VitalyKulenko <vitalykulenko@mail.ru>
2020-06-22 11:52:32 +0300

    Поправил баг в Firefox, но теперь чат постоянно вверху

--- js/consultation.js
+++ js/consultation.js
@@ -1,4 +1,14 @@
 $(document).ready(() => {
     $('#message-2 .like').css('display', 'none');
     $('#message-2 .like-check').css('display', 'inline');
+
+    $('#chat-tab').click(() => {
+        $('#send-form-dialogue').css('display', 'none');
+        $('#send-form-chat').css('display', 'flex');
+    });
+
+    $('#dialogue-tab').click(() => {
+        $('#send-form-dialogue').css('display', 'flex');
+        $('#send-form-chat').css('display', 'none');
+    });
 });
